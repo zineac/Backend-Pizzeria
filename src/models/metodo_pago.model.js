5c315d826a995021c5ba03f39616d694c13cae9d
vicente <arckzineac@gmail.com>
2025-11-04 14:58:38 -0500

    feat(modelo): agregar campo activo en MetodoPago

--- src/models/metodo_pago.model.js
+++ src/models/metodo_pago.model.js
@@ -4,7 +4,8 @@ import { DataTypes } from 'sequelize'
 export const MetodoPago = db.define('MetodoPago', {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
   tipo: { type: DataTypes.STRING(50), allowNull: false },
-  descripcion: { type: DataTypes.STRING(100) }
+  descripcion: { type: DataTypes.STRING(100) },
+  activo: { type: DataTypes.BOOLEAN, defaultValue: true }
 }, {
   tableName: 'metodos_pago',
   timestamps: false

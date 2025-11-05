6c3a93b7804f0c2a548be95b921bbc6f05b72936
vicente <arckzineac@gmail.com>
2025-11-05 10:25:37 -0500

    feat(models): agregar campo 'activo' a Pedido y Rol para borrado lógico

--- src/models/pedido.model.js
+++ src/models/pedido.model.js
@@ -5,10 +5,11 @@ export const Pedido = db.define('Pedido', {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
   id_cliente: { type: DataTypes.INTEGER, allowNull: false },
   id_repartidor: { type: DataTypes.INTEGER },
-  metodo_pago: { type: DataTypes.INTEGER },
+  id_metodo_pago: { type: DataTypes.INTEGER },
   estado: { type: DataTypes.INTEGER, defaultValue: 1 },
   fecha_pedido: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
-  total: { type: DataTypes.DECIMAL(10, 2), defaultValue: 0.00 }
+  total: { type: DataTypes.DECIMAL(10, 2), defaultValue: 0.00 },
+  activo: { type: DataTypes.BOOLEAN, defaultValue: true }
 }, {
   tableName: 'pedidos',
   timestamps: false
--- src/models/rol.model.js
+++ src/models/rol.model.js
@@ -3,7 +3,8 @@ import { DataTypes } from 'sequelize'
 
 export const Rol = db.define('Rol', {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  nombre: { type: DataTypes.STRING(50), allowNull: false }
+  nombre: { type: DataTypes.STRING(50), allowNull: false },
+  activo: { type: DataTypes.BOOLEAN, defaultValue: true }
 }, {
   tableName: 'roles',
   timestamps: false

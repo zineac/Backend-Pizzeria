49fa76445fef9819895171b350b34ad34ae9a737
vicente <arckzineac@gmail.com>
2025-11-05 10:08:42 -0500

    feat(model): agregar campo activo al modelo Tamano

--- src/models/tamano.model.js
+++ src/models/tamano.model.js
@@ -4,7 +4,8 @@ import { DataTypes } from 'sequelize'
 export const Tamano = db.define('Tamano', {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
   nombre: { type: DataTypes.STRING(50), allowNull: false },
-  factor_precio: { type: DataTypes.DECIMAL(4, 2), defaultValue: 1.00 }
+  factor_precio: { type: DataTypes.DECIMAL(4, 2), defaultValue: 1.00 },
+  activo: { type: DataTypes.BOOLEAN, defaultValue: true }
 }, {
   tableName: 'tamanos',
   timestamps: false
